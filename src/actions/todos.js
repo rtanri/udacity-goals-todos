10ee2d0f16840e278c958455922be53956f77943
Revin Tanri <tanrirevin@gmail.com>
2020-05-09 16:14:12 +0800

    Put in Action items in Todos.js

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -3,3 +3,23 @@ export const ADD_TODO = 'ADD_TODO'
 export const REMOVE_TODO = 'REMOVE_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 
+function addTodoAction (todo) {
+    return {
+        type: ADD_TODO,
+        todo,
+    }
+}
+
+function removeTodoAction (id) {
+    return {
+        type: REMOVE_TODO,
+        id,
+    }
+}
+
+function toggleTodoAction (id) {
+    return {
+        type: TOGGLE_TODO,
+        id,
+    }
+}
