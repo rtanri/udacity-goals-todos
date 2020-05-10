a133828c469e4b1f9120366a235c75cbf7a050de
Revin Tanri <tanrirevin@gmail.com>
2020-05-10 10:51:52 +0800

    Import few items into main index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import reducer from './reducers' // 1. Add root reducer 
+import middleware from './middleware'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore(reducer, middleware)
 
 
 ReactDOM.render(
+  <Provider store={store}>
     <App />,
+  </Provider>,
   document.getElementById('root')
 )
